7a3fc81d447235b28ff33394a2d2740564f7f4d7
Peter Essam <petros.guirguis@hotmail.com>
2023-04-09 15:14:53 +0200

    add check box

--- user_reg.php
+++ user_reg.php
@@ -5,19 +5,22 @@
 </head>
 <body>
     <h1>User Registration</h1>
-    <form method="post" action="user_list1.php">
+    <form method="post" action="user_list.php">
         <label for="name">Name:</label>
         <input type="text" id="name" name="name" required><br>
 
         <label for="email">Email:</label>
         <input type="email" id="email" name="email" required><br>
 
-        <label>Gender:</label>
-        <label for="male">Male</label>
+        <label for="gender">Gender:</label>
         <input type="radio" id="male" name="gender" value="male" required>
+        <label for="male">Male</label>
 
-        <label for="female">Female</label>
-        <input type="radio" id="female" name="gender" value="female" required><br>
+        <input type="radio" id="female" name="gender" value="female" required>
+        <label for="female">Female</label><br>
+
+        <label for="mail">Mail:</label>
+        <input type="checkbox" id="mail" name="mail" value="subscribe"> Subscribe<br>
 
         <button type="submit">Submit</button>
     </form>
